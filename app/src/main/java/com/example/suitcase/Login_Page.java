e9ebd52960bf95dbd78ad3bd090f63234e3f108a
Rahul Kumar <rahulloni000@gmail.com>
2023-08-21 08:42:52 +0545

    signup and login

--- app/src/main/java/com/example/suitcase/Login_Page.java
+++ app/src/main/java/com/example/suitcase/Login_Page.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.suitcase.databinding.ActivityLoginPageBinding;
 
@@ -23,7 +24,21 @@ public class Login_Page extends AppCompatActivity {
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email=binding.txtLoginEmail.getText().toString().trim();
+                String password=binding.txtLoginPassword.getText().toString().trim();
 
+                if(email.equals("") || password.equals("")){
+                    Toast.makeText(Login_Page.this, "All field are mandatory", Toast.LENGTH_SHORT).show();
+                }else {
+                    Boolean checkCredential=databaseHelper.checkEmailPassword(email,password);
+                    if (checkCredential==true){
+                        Toast.makeText(Login_Page.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }else {
+                        Toast.makeText(Login_Page.this, "Invalid Credential", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
         binding.txtForgotPassword.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/suitcase/Signup_Page.java
+++ app/src/main/java/com/example/suitcase/Signup_Page.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.suitcase.databinding.ActivitySignupPageBinding;
 
@@ -23,6 +24,38 @@ DatabaseHelper databaseHelper;
         binding.signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Get Text Method
+                String email=binding.txtSignupEmail.getText().toString().trim();
+                String password=binding.txtSignupPassword.getText().toString().trim();
+                String Cpassword=binding.txtSignupCpassword.getText().toString().trim();
+
+                //Form Validation
+
+                if (email.equals("")  || password.equals("") || Cpassword.equals("")){
+                    Toast.makeText(Signup_Page.this, "All field are mandatory", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                      if (password.equals(Cpassword)){
+                         Boolean checkEmail=databaseHelper.checkEmail(email);
+                         if (checkEmail==false){
+                             Boolean insert=databaseHelper.insertUsers(email,password);
+                             if (insert==true){
+                                 Toast.makeText(Signup_Page.this, "Signup Comp", Toast.LENGTH_SHORT).show();
+                                 Intent intent=new Intent(getApplicationContext(),Login_Page.class);
+                                 startActivity(intent);
+                             }else {
+                                 Toast.makeText(Signup_Page.this, "Signup Failed", Toast.LENGTH_SHORT).show();
+                             }
+                         }
+                         else {
+                             Toast.makeText(Signup_Page.this, "Users Already exists", Toast.LENGTH_SHORT).show();
+                         }
+                      }
+                      else {
+                          Toast.makeText(Signup_Page.this, "Invalid Password ", Toast.LENGTH_SHORT).show();
+                      }
+
+                }
                 //Signup Method
             }
         });
