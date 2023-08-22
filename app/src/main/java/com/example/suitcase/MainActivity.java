bdec282f33d15ceebe820edb6ca7aa373e3053c2
Rahul Kumar <rahulloni000@gmail.com>
2023-08-22 07:57:49 +0545

    design main and add items activity

--- app/src/main/java/com/example/suitcase/MainActivity.java
+++ app/src/main/java/com/example/suitcase/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.suitcase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.suitcase.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Add_Items.class);
+                startActivity(intent);
+            }
+        });
     }
 }
