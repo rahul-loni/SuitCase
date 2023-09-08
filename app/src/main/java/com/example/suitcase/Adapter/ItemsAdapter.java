70e0175c3721ef75993258e84eef5a497804ee3b
Rahul Kumar <rahulloni000@gmail.com>
2023-09-08 09:46:46 +0545

    Item model and itemsAdapter

--- app/src/main/java/com/example/suitcase/Adapter/ItemsAdapter.java
+++ app/src/main/java/com/example/suitcase/Adapter/ItemsAdapter.java
@@ -1,16 +1,34 @@
 package com.example.suitcase.Adapter;
 
+import android.content.ClipData;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.suitcase.ItemsModel;
+import com.example.suitcase.R;
+
+import java.util.ArrayList;
+
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
+    private final RecyclerItemsClickView recyclerItemsClickView;
+    private ArrayList<ItemsModel> itemsModels;
+
+    public ItemsAdapter(ArrayList<ItemsModel>itemsModels,RecyclerItemsClickView recyclerItemsClickView){
+        this.recyclerItemsClickView=recyclerItemsClickView;
+        this.itemsModels=itemsModels;
+    }
+
     @NonNull
     @Override
-    public ItemsAdapter.ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.single_item,parent,false);
+        return new ItemViewHolder(view);
     }
 
     @Override
---
+++ app/src/main/java/com/example/suitcase/ItemsModel.java
@@ -0,0 +1,75 @@
+package com.example.suitcase;
+
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
+public class ItemsModel {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private Uri image;
+    private boolean purchased;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Uri getImage() {
+        return image;
+    }
+
+    public void setImage(Uri image) {
+        this.image = image;
+    }
+
+    public boolean isPurchased() {
+        return purchased;
+    }
+
+    public void setPurchased(boolean purchased) {
+        this.purchased = purchased;
+    }
+        public String toString () {
+            return "Item{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", description='" + description + '\'' +
+                    ", image=" + image +
+                    ", price=" + price +
+                    ", purchased=" + purchased +
+                    '}';
+        }
+    }
+
